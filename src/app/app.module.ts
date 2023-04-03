2790b51814a48271d1b8f183c685d4782afebff5
Vinny <vinny@cubetechnology.org>
2023-04-03 18:17:06 -0400

    Now using "Angular Material" on the App (Step 8)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,13 @@ import { XyzComponent } from './xyz/xyz.component';
 import { MissionlistComponent } from './missionlist/missionlist.component';
 import { MissionfilterComponent } from './missionfilter/missionfilter.component';
 import { MissiondetailsComponent } from './missiondetails/missiondetails.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+/* Import Angular Material */
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -22,7 +29,12 @@ import { MissiondetailsComponent } from './missiondetails/missiondetails.compone
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
